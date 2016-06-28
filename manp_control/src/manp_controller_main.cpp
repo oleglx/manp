745592ecde121192661d931f5d63fbdfcd0f5c64
oleglx <olvarg@mail.ru>
2016-06-28 15:07:13 +0300

    Milestone. Programm compiles and launches with ros.

--- manp_control/src/manp_controller_main.cpp
+++ manp_control/src/manp_controller_main.cpp
@@ -4,9 +4,10 @@
 #include<ros/ros.h>
 #include<std_msgs/Float64.h>
 #include<std_msgs/String.h>
+#include<sensor_msgs/JointState.h>
 
 std::vector<float> joint_positions(3), joint_efforts(3);
-const std::vector<float> link_lengths(3), base_point(3), destination(3);
+std::vector<float> link_lengths(3), base_point(3), destination(3);
 
 class Manipulator {
 	private:
@@ -15,7 +16,8 @@ class Manipulator {
 
 		void updateManipulator();
 	public:
-		Manipulator( std::vector<float> manp_base_point, std::vector<float> manp_angles, std::vector<float> manp_link_lengths ) {
+		Manipulator( std::vector<float> manp_base_point, std::vector<float> manp_angles, std::vector<float> manp_link_lengths ) : base_point(3), angles(3), link_lengths(3), point_zero(3), point_one(3), point_two(3) { 
+
 			setManipulator( manp_base_point, manp_angles, manp_link_lengths );		
 		}
 		
@@ -25,44 +27,63 @@ class Manipulator {
 		void transform(std::vector<float>& vect, float angle);
 		void atransform(std::vector<float>& vect, float angle);		
 		void FABRIK( int iter, std::vector<float> dest );
-		bool checkDestination ( std::vector<float> joint_positions(3) );
+		bool checkDestination ( std::vector<float> joint_positions );
+		void controlSynth ( std::vector<float> joint_positions, std::vector<float>& control );
 };
 
+int initialize_globals(){  
+	base_point[0] = 0;
+	base_point[1] = 0;
+	base_point[2] = 0;
+
+	link_lengths[0] = 0;
+	link_lengths[1] = 4;
+	link_lengths[2] = 4;
+
+	destination[0] = 4;
+	destination[1] = 4;
+	destination[2] = 4;
+}
 
 void joints_callback( const sensor_msgs::JointState::ConstPtr& inp_msg ) {
-	vector<sensor_msgs::JointState>::iterator it1;
+	std::vector<std::string>::const_iterator it1;
 
 	it1 = find(inp_msg->name.begin(),inp_msg->name.end(),"world_to_base");
 	if (it1==inp_msg->name.end()) {
-		fprintf("stderr", "world_to_base not found \n");
+		fprintf(stderr, "world_to_base not found \n");
 	}
 	else {
-		joint_positions[0] = it1->position;
-		joint_efforts[0] = it1->effort;
+		joint_positions[0] = inp_msg->position[it1 - inp_msg->name.begin()];
+                
+		joint_efforts[0] = inp_msg->effort[it1 - inp_msg->name.begin()];
 	}
 
 	it1 = find(inp_msg->name.begin(),inp_msg->name.end(),"link1_to_link2");
 	if (it1==inp_msg->name.end()) {
-		fprintf("stderr", "link1_to_link2 not found \n");
+		fprintf(stderr, "link1_to_link2 not found \n");
 	}
 	else {
-		joint_positions[1] = it1->position;
-		joint_efforts[1] = it1->effort;
+		joint_positions[1] = inp_msg->position[it1 - inp_msg->name.begin()];
+                joint_efforts[1] = inp_msg->effort[it1 - inp_msg->name.begin()];
 	}
 
 	it1 = find(inp_msg->name.begin(),inp_msg->name.end(),"link2_to_link3");
 	if (it1==inp_msg->name.end()) {
-		fprintf("stderr", "link2_to_link3 not found \n");
+		fprintf(stderr, "link2_to_link3 not found \n");
 	}
 	else {
-		joint_positions[2] = it1->position;
-		joint_efforts[2] = it1->effort;
+		joint_positions[2] = inp_msg->position[it1 - inp_msg->name.begin()];
+                joint_efforts[2] = inp_msg->effort[it1 - inp_msg->name.begin()];
 	}
 
 }
 
 
 int main( int argc, char **argv ) {
+	std::vector<float> control(3);
+
+	initialize_globals();	
+	
 	ros::Publisher wtb_pub, l1tl2_pub, l2tl3_pub;
 
 	ros::init(argc, argv, "manp_controller");
@@ -73,30 +94,28 @@ int main( int argc, char **argv ) {
 	l2tl3_pub = n.advertise<std_msgs::Float64>("/manp/link2_to_link3_state_controller/command", 50);
 
 	ros::Subscriber sub_coordinates = n.subscribe("/manp/joint_state", 50, joints_callback);
-
 	ros::Rate r(50);
 	ros::spinOnce();
 
 	Manipulator model(base_point, joint_positions, link_lengths);
 	model.FABRIK(10, destination);
-	model.updateManipulator();	
 
 	r.sleep();
 	while(ros::ok() & model.checkDestination(joint_positions) == false ) {
 		
 		ros::spinOnce();
-		
-		
 
 
-
+		model.controlSynth(joint_positions, control);
 
 		std_msgs::Float64 out_msg;
-		out_msg.data = 0.0;
+		out_msg.data = control[0];
 		wtb_pub.publish(out_msg);
-		out_msg.data = 0.0;
+
+		out_msg.data = control[1];
 		l1tl2_pub.publish(out_msg);
-		out_msg.data = 0.0;
+
+		out_msg.data = control[2];
 		l2tl3_pub.publish(out_msg);
     		
     		r.sleep();
@@ -113,16 +132,20 @@ void Manipulator::updateManipulator() {
 	base_point[0] = point_zero[0];
 	base_point[1] = point_zero[1];
 	base_point[2] = point_zero[2];
+
+	//Setting world_to_base angle
+	angles[0] = atan( (point_one[1] - point_zero[1])/(point_one[0] - point_zero[0]) );
 		
 	//Setting link1_to_link2 angle
-	angles[1] = atan( (point_one[0] - point_zero[0])/(point_one[2] - point_zero[2]) );
+	angles[1] = acos( (point_one[2] - point_zero[2])/link_lengths[1]);
 
 	//Setting link2_to_link3 angle
-	angles[2] = atan( (point_two[0] - point_one[0])/(point_two[2] - point_one[2]) );
+	angles[2] = acos( (point_two[2] - point_one[2])/link_lengths[2]);
 }
 
 
 void Manipulator::setManipulator( std::vector<float> manp_base_point, std::vector<float> manp_angles, std::vector<float> manp_link_lengths ) {
+
 	base_point = manp_base_point;
 	angles = manp_angles;
 	link_lengths = manp_link_lengths;
@@ -135,9 +158,9 @@ void Manipulator::setManipulator( std::vector<float> manp_base_point, std::vecto
 	point_one[1] = base_point[1] + link_lengths[1]*sin(angles[0])*sin(angles[1]);
 	point_one[2] = base_point[2] + link_lengths[1]*cos(angles[1]);
 
-	point_two[0] = point_one[0] + link_lengths[1]*cos(angles[0])*sin(angles[2]);	
-	point_two[1] = point_one[1] + link_lengths[1]*sin(angles[0])*sin(angles[2]);
-	point_two[2] = point_one[2] + link_lengths[1]*cos(angles[2]);
+	point_two[0] = point_one[0] + link_lengths[2]*cos(angles[0])*sin(angles[2]);	
+	point_two[1] = point_one[1] + link_lengths[2]*sin(angles[0])*sin(angles[2]);
+	point_two[2] = point_one[2] + link_lengths[2]*cos(angles[2]);
 }
 
 void Manipulator::getManipulator( std::vector<float>& manp_base_point, std::vector<float>& manp_angles, std::vector<float>& manp_link_lengths ) {
@@ -147,9 +170,9 @@ void Manipulator::getManipulator( std::vector<float>& manp_base_point, std::vect
 }
 
 void Manipulator::getPoint ( float link_length, std::vector<float> stationary, std::vector<float> moving, std::vector<float>& out ) {
-	std::vector<float> vect;
+	std::vector<float> vect(2);
 
-	vect[0] = moving[1] - stationary[1];
+	vect[0] = moving[0] - stationary[0];
 	vect[1] = moving[2] - stationary[2];
 
 	float k = pow(pow(vect[0],2) + pow(vect[1],2), 0.5);
@@ -157,7 +180,7 @@ void Manipulator::getPoint ( float link_length, std::vector<float> stationary, s
 	vect[0] /= k;
 	vect[1] /= k;
 	
-	out[1] = stationary[1] + link_length*vect[0];
+	out[1] = stationary[0] + link_length*vect[0];
 	out[2] = stationary[2] + link_length*vect[1];
 }
 
@@ -175,18 +198,23 @@ void Manipulator::atransform(std::vector<float>& vect, float angle) {
 }
 
 void Manipulator::FABRIK( int iter, std::vector<float> dest ) {
-	std::vector<float> zero_pv, one_pv, two_pv;
+	std::vector<float> zero_pv(3), one_pv(3), two_pv(3);
+	float rot_angle;
 
-	angles[0] = atan( (dest[1] - point_zero[1])/(dest[0] - point_zero[0]) );
-	
+	rot_angle = acos( (point_one[0]*dest[0] + point_two[1]*dest[1])/( link_lengths[1]*link_lengths[2]*sin(angles[1])*sin(angles[2]) ) );
+	      
 	//Rotating vectors to destination
-	transform(point_one, angles[0]);
-	transform(point_two, angles[0]);
+	transform(point_one, rot_angle);
+	transform(point_two, rot_angle);
 
 	//Transforming to another coordinate system
-	transform(point_one, angles[0]);
-	transform(point_two, angles[0]);
-								
+	transform(point_one, rot_angle);
+	transform(point_two, rot_angle);
+	     
+
+	//----------------ATTENTION-------------------------				
+	//SOLUTION NOW IS IN THE LOCAL SYSTEM OF COORDINATES
+				
 	for ( int i = 0; i < iter/2; i++ ) {
 	//Staring reverse pass				
 		zero_pv = point_zero;
@@ -201,7 +229,7 @@ void Manipulator::FABRIK( int iter, std::vector<float> dest ) {
 			
 		//Moved point ZERO for the reverse pass
 		getPoint( link_lengths[1], point_one, zero_pv, point_zero);
-						
+			
 	//Starting direct pass
 		one_pv = point_one;			
 		two_pv = point_two;
@@ -215,13 +243,25 @@ void Manipulator::FABRIK( int iter, std::vector<float> dest ) {
 		//Moved point point_two for the direct pass
 		getPoint( link_lengths[2], point_one, two_pv, point_two );
 	}
+	      
+
 	//Transforming to basic coordinate system
-	atransform(point_one, angles[0]);
-	atransform(point_two, angles[0]);
+	atransform(point_one, rot_angle);
+	atransform(point_two, rot_angle);
+
+	//----------------ATTENTION-------------------------				
+	//SOLUTION NOW IS IN THE GLOBAL SYSTEM OF COORDINATES
+
+	updateManipulator();
 }
 
 bool Manipulator::checkDestination ( std::vector<float> joint_positions  ) {
-	if (joint_positions == angles)
-		return true;
-	else return false;
+	return (joint_positions == angles);
+}
+
+void Manipulator::controlSynth ( std::vector<float> joint_positions, std::vector<float>& control ) {
+	float k = 0.1;
+	for (int i = 0; i < 3; i++) {
+		control[i] = k*(angles[i] - joint_positions[i]);
+	}
 }
